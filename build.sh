3af67d48a2a59815ccf5e7c412e1760b4fb71bcf
Anush Kumar <anushkumarkorani@gmail.com>
2025-03-12 23:40:52 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Install yt-dlp
-sudo apt-get update
-sudo apt-get install -y python3-pip
-sudo pip3 install yt-dlp
+apt-get update
+apt-get install -y python3-pip
+pip3 install yt-dlp
 
 # Install ffmpeg
-sudo apt-get install -y ffmpeg
+apt-get install -y ffmpeg
 
 # Install Node.js dependencies
 npm install
