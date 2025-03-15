51283fccd18b6da72e58e38fe9803ade43e2aecd
Anush Kumar <anushkumarkorani@gmail.com>
2025-03-15 17:55:03 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,12 +2,9 @@
 # Install yt-dlp
 pip3 install yt-dlp
 
-# Download and install ffmpeg
-wget https://github.com/yt-dlp/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl.tar.xz
-tar -xf ffmpeg-master-latest-linux64-gpl.tar.xz
-mv ffmpeg-master-latest-linux64-gpl/bin/ffmpeg /usr/local/bin/
-mv ffmpeg-master-latest-linux64-gpl/bin/ffprobe /usr/local/bin/
-rm -rf ffmpeg-master-latest-linux64-gpl ffmpeg-master-latest-linux64-gpl.tar.xz
+# Install ffmpeg
+sudo apt-get update
+sudo apt-get install -y ffmpeg
 
 # Install Node.js dependencies
 npm install
